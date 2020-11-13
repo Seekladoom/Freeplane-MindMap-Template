4b9764ef190544ce59eeed2f33a8443b9e856971
Seekladoom <35969469+Seekladoom@users.noreply.github.com>
2020-11-13 10:13:53 +0800

    Update 仿XMind：稳重 向左.mm

--- template/Seekladoom/仿XMind：稳重 向左.mm
+++ template/Seekladoom/仿XMind：稳重 向左.mm
@@ -5,102 +5,102 @@
 
 <map_styles>
 <stylenode LOCALIZED_TEXT="styles.root_node" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
-<font NAME="更纱黑体 SC Semibold" SIZE="24"/>
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="24"/>
 <edge STYLE="horizontal" COLOR="#35455b"/>
 <stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
-<font NAME="更纱黑体 SC Semibold"/>
+<font NAME="Sarasa Gothic PDF SC Semibold"/>
 <edge STYLE="horizontal" COLOR="#35455b"/>
 <stylenode LOCALIZED_TEXT="default" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
-<font NAME="更纱黑体 SC Semibold" SIZE="10" BOLD="false" ITALIC="false"/>
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10" BOLD="false" ITALIC="false"/>
 <edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
-<font NAME="更纱黑体 SC Semibold"/>
+<font NAME="Sarasa Gothic PDF SC Semibold"/>
 <edge STYLE="horizontal" COLOR="#35455b"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.attributes" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
-<font NAME="更纱黑体 SC Semibold" SIZE="9"/>
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="9"/>
 <edge STYLE="horizontal" COLOR="#35455b"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" TEXT_ALIGN="LEFT">
-<font NAME="更纱黑体 SC Semibold"/>
+<font NAME="Sarasa Gothic PDF SC Semibold"/>
 <edge STYLE="horizontal" COLOR="#35455b"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.floating" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
-<font NAME="更纱黑体 SC Semibold"/>
+<font NAME="Sarasa Gothic PDF SC Semibold"/>
 <edge STYLE="horizontal" COLOR="#35455b"/>
 </stylenode>
 </stylenode>
 <stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
-<font NAME="更纱黑体 SC Semibold"/>
+<font NAME="Sarasa Gothic PDF SC Semibold"/>
 <edge STYLE="horizontal" COLOR="#35455b"/>
 <stylenode LOCALIZED_TEXT="styles.topic" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
-<font NAME="更纱黑体 SC Semibold" SIZE="10" BOLD="true"/>
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10" BOLD="true"/>
 <edge STYLE="horizontal" COLOR="#35455b"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
-<font NAME="更纱黑体 SC Semibold" SIZE="10" BOLD="true"/>
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10" BOLD="true"/>
 <edge STYLE="horizontal" COLOR="#35455b"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
-<font NAME="更纱黑体 SC Semibold" SIZE="10" BOLD="true"/>
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10" BOLD="true"/>
 <edge STYLE="horizontal" COLOR="#35455b"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="styles.important" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
 <icon BUILTIN="yes"/>
-<font NAME="更纱黑体 SC Semibold"/>
+<font NAME="Sarasa Gothic PDF SC Semibold"/>
 <edge STYLE="horizontal" COLOR="#35455b"/>
 </stylenode>
 </stylenode>
 <stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" COLOR="#ffffff" BACKGROUND_COLOR="#ffffff" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt">
-<font NAME="更纱黑体 SC Semibold"/>
+<font NAME="Sarasa Gothic PDF SC Semibold"/>
 <edge STYLE="horizontal" COLOR="#35455b"/>
 <stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#35455b" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
-<font NAME="更纱黑体 SC Semibold" SIZE="18"/>
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="18"/>
 <edge STYLE="horizontal" COLOR="#35455b" WIDTH="3" DASH="SOLID"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ffffff" BACKGROUND_COLOR="#84a1c9" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" FORMAT="#" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#05a8f4" BORDER_COLOR_ALPHA="0">
-<font NAME="更纱黑体 SC Semibold" SIZE="18"/>
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="18"/>
 <edge STYLE="horizontal" COLOR="#35455b" WIDTH="3" DASH="SOLID"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
-<font NAME="更纱黑体 SC Semibold" SIZE="10"/>
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
 <edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
-<font NAME="更纱黑体 SC Semibold" SIZE="10"/>
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
 <edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
-<font NAME="更纱黑体 SC Semibold" SIZE="10"/>
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
 <edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
-<font NAME="更纱黑体 SC Semibold" SIZE="10"/>
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
 <edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
-<font NAME="更纱黑体 SC Semibold" SIZE="10"/>
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
 <edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
-<font NAME="更纱黑体 SC Semibold" SIZE="10"/>
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
 <edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
-<font NAME="更纱黑体 SC Semibold" SIZE="10"/>
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
 <edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
-<font NAME="更纱黑体 SC Semibold" SIZE="10"/>
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
 <edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
-<font NAME="更纱黑体 SC Semibold" SIZE="10"/>
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
 <edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" COLOR="#000000" BACKGROUND_COLOR="#e6e6e6" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5.0 pt" SHAPE_VERTICAL_MARGIN="3.0 pt" BORDER_WIDTH="1.0 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#808080" BORDER_COLOR_ALPHA="0">
-<font NAME="更纱黑体 SC Semibold" SIZE="10"/>
+<font NAME="Sarasa Gothic PDF SC Semibold" SIZE="10"/>
 <edge STYLE="horizontal" COLOR="#35455b" WIDTH="1" DASH="SOLID"/>
 </stylenode>
 </stylenode>
